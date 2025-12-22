f2a503f5dc91ce52c21d26d2b360e88ea57d4303
Rafahele22 <fisky@hotmail.de>
2025-12-22 14:55:42 +0100

    Server test

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ function sendHtml(res, code, html) {
 
 const server = http.createServer(async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     if (req.method === 'OPTIONS') {
